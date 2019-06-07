0955f23c366777572b82108f5b9fb70319048eaf
fsedanoc <fran@fransedano.net>
2019-06-07 23:30:40 +0200

    Changes

--- rest-client/target/rest-server.py
+++ rest-client/target/rest-server.py
@@ -10,110 +10,24 @@ app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
-class WLC:
-    def __init__(self, controller_ip):
-        self.controller_ip = controller_ip
-        self.headers = {
-            'Accept': "application/yang-data+json",
-            'Content-Type': "application/yang-data+json",
-            'Authorization': "Basic Y2lzY286Y2lzY28=",
-            #'Authorization': "Basic YWRtaW46VmltbGFiMTIzCg==",
-            'cache-control': "no-cache"
-        }
-        self.url = f"https://{self.controller_ip}/restconf/data/Cisco-IOS-XE-wireless-ap-cfg:ap-cfg-data/ap-tags/ap-tag/"
-
-    def _change_policy_tag_payload(self, payload, mac, new_policy_tag):
-        _payload = json.loads(payload.text)
-        print(_payload)
-        for entry in _payload['Cisco-IOS-XE-wireless-ap-cfg:ap-tag']:
-            if entry['ap-mac'] == mac:
-                entry['policy-tag'] = new_policy_tag
-        return json.dumps(_payload)
-
-    def move_ap(self, mac, tag):
-        payload = requests.request("GET", self.url, headers=self.headers, verify=False)
-        print("Got config from Controller:")
-        print(payload.text)
-        if True:
-            print(f"Moving AP:{mac} to tag:{tag}")
-            payload = self._change_policy_tag_payload(payload, mac, tag)
-            response = requests.request("PATCH", self.url, data=payload, headers=self.headers, verify=False)
-            print(response.text)
-            print("Got config from Controller, after update:")
-            response = requests.request("GET", self.url, headers=self.headers, verify=False)
-            print(response.text)
-        return {'Message':'Successfully changed radio'}
-
-def post_util(ap, tag):
-        wlc = WLC("9.9.71.50")
-        #wlc = WLC("18.184.63.134")
-        try:
-            ret = wlc.move_ap(ap, tag)
-        except ConnectionError:
-            ret = {'failed': True}
-        return ret
-
-class startAP1(Resource):
-    def post(self):
-        #ap = "00:a6:ca:6c:d5:90"
-        #ap = "00:2c:c8:8b:72:d0"
-        #ap = "2c:33:11:85:fe:40"
-        ap = "00:42:68:c5:c7:4e"
-        tag = "radioup"
-        print("Start AP1: {}".format(ap))
-        return post_util(ap, tag)
-
-class stopAP1(Resource):
-    def post(self):
-        #ap = "00:a6:ca:6c:d5:90"
-        #ap = "00:2c:c8:8b:72:d0"
-        #ap = "2c:33:11:85:fe:40"
-        ap = "00:42:68:c5:c7:4e"
-        tag = "radiodown"
-        print("Stop AP1: {}".format(ap))
-        return post_util(ap, tag)
-
-class startAP2(Resource):
-    def post(self):
-        ap = "f4:4e:05:43:34:54"
-        #ap = "2c:33:11:85:fe:40"
-        #ap = "58:ac:78:dc:f5:b0"
-        tag = "radioup"
-        print("Start AP1: {}".format(ap))
-        return post_util(ap, tag)
-
-class stopAP2(Resource):
-    def post(self):
-        ap = "f4:4e:05:43:34:54"
-        #ap = "2c:33:11:85:fe:40"
-        #ap = "58:ac:78:dc:f5:b0"
-        tag = "radiodown"
-        print("Stop AP1: {}".format(ap))
-        return post_util(ap, tag)
 
 class GetCLI(Resource):
     def get(self):
-        cisco = {
-        'device_type': 'cisco_ios',
-        'host': '10.51.65.167',
-        'username': 'lab',
-        'password': 'lab'
-        }
-        #data = [{'intf': 'GigabitEthernet1', 'ipaddr': '10.0.0.1', 'status': 'up', 'proto': 'up'},
-        #{'intf': 'GigabitEthernet2', 'ipaddr': '11.0.0.1', 'status': 'up', 'proto': 'up'}]
-        #print(data)
-        #return data
         net_connect = ConnectHandler(**cisco)
-        output = net_connect.send_command("show ip int brief", use_textfsm=True)
+        output = net_connect.send_command("show ap summary", use_textfsm=True)
         net_connect.disconnect()
-        print(f"Output is {output}")
+        print(f"Output is {output}")        
         return output
 
-api.add_resource(startAP1, '/start_ap1')
-api.add_resource(stopAP1, '/stop_ap1')
-api.add_resource(startAP2, '/start_ap2')
-api.add_resource(stopAP2, '/stop_ap2')
-api.add_resource(GetCLI, '/metrics')
 
+cisco = {
+'device_type': 'cisco_ios',
+'host': '192.168.30.205',
+'username': 'lab',
+'password': 'lab'
+}
+
+api.add_resource(GetCLI, '/metrics')
 app.run(host='0.0.0.0', port='4000', debug=True)
+
 
